4453f5e1dbf7f8190f43c99ebfbcdd3a2226d287
Charmaine Simangaliso Mangorima <82668352+charmainemimie@users.noreply.github.com>
2025-07-18 22:00:53 +0200

    Updated Phone Number in contact-page.tsx

--- src/pages/contact-page.tsx
+++ src/pages/contact-page.tsx
@@ -47,7 +47,7 @@ export default function ContactPage() {
                   </div>
                   <div className="flex items-center gap-3 text-cream-light">
                     <Phone className="w-5 h-5 text-gold-accent" />
-                    <span>++27 67 244 3534</span>
+                    <span>+27 67 244 3534</span>
                   </div>
                 </div>
               </CardContent>
